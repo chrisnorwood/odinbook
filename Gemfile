2d6d3320970b167421e100a9c7e0ddd934c86e24
Chris Norwood <cnorwood92@gmail.com>
2016-10-16 13:00:08 -0700

    basic devise setup and configuration

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'figaro'
+gem 'devise'
+gem 'omniauth-facebook'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,12 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Devise told me to add this ...
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Use letter opener for development emails
+  config.action_mailer.delivery_method = :letter_opener
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
