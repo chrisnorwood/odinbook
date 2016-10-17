cba7f5799b5e43b8f65337a8ed52a41b9adc1de0
Chris Norwood <cnorwood92@gmail.com>
2016-10-16 19:43:48 -0700

    Update views and routings for basic devise stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  devise_for :users
+
+  devise_for :user, controllers: {  
+    # confirmations: 'users/confirmations',
+    passwords: 'devise/passwords',
+    registrations: 'devise/registrations',
+    sessions: 'devise/sessions',
+    # unlocks: 'users/unlocks',
+  }, skip: [:sessions]
+
+  ## custom routes for users
+  as :user do
+    get 'login' => 'devise/sessions#new', as: :new_user_session
+    post 'login' => 'devise/sessions#create', as: :user_session
+    delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
+    get 'register' => 'devise/registrations#new'
+  end
 end
