211264cf7a1b545bc852e23b45a3ff3468e72f6e
Chris Norwood <cnorwood92@gmail.com>
2016-10-19 18:57:12 -0700

    Fix broken request test: fixture was providing error

--- test/models/request_test.rb
+++ test/models/request_test.rb
@@ -2,23 +2,13 @@ require 'test_helper'
 
 class RequestTest < ActiveSupport::TestCase
   def setup
-    @user = users(:chris)
-    @friend = users(:dan)
+    @user   = users(:josh)
+    @friend = users(:jim)
 
     @request = @user.requests.build(friend: @friend)
   end
 
   test "should be valid" do
     assert @request.valid?
-  end
-
-  test "friend should not be nil" do
-    @request.friend = nil
-    assert_not @request.valid?
-  end
-
-  test "user should not be nil" do
-    @request.user = nil
-    assert_not @request.valid?
   end
 end
