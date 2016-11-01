bc79044429f603c41c818567655ee4d4cc68a671
Chris Norwood <cnorwood92@gmail.com>
2016-11-01 16:35:21 -0700

    Rename before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :correct_user, only: :set_profile_picture
+  before_action :correct_user, only: :set_profile_photo
 
   def show
     @user = User.find(params[:id])
