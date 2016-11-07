238b5e6d82856efb36924b11cdcb0dbdf16ea4c1
Chris Norwood <cnorwood92@gmail.com>
2016-11-07 11:42:23 -0800

    Mailer works in production with letter_opener

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  
+  before_action :configure_permitted_parameters
+
+  def create
+    super
+    if @user.persisted?
+      UserMailer.welcome(@user).deliver_now
+    end
+  end
+
   # Overwrite update_resource to let users to update their user without giving their password
   def update_resource(resource, params)
     if current_user.provider == "facebook"
@@ -9,4 +17,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
       resource.update_with_password(params)
     end
   end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    end
 end
